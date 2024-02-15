5e915c5a983d3473f0f3fd2e3899baac2eabffa1
paul-hyun <cchyun@gmail.com>
2024-02-16 01:18:49 +0900

    add unity arm

--- unity_ws/Assets/jetauto_description/CarController.cs
+++ unity_ws/Assets/jetauto_description/CarController.cs
@@ -8,7 +8,7 @@ public class CarController : MonoBehaviour
     private const string HORIZONTAL = "Horizontal";
     private const string VERTICAL = "Vertical";
 
-    private float horizontalInput;
+    private float[] inputs = new float[7];
     private float verticalInput;
 
     [SerializeField] private float motorForce = 10f;
@@ -18,6 +18,12 @@ public class CarController : MonoBehaviour
     [SerializeField] private Transform viewFR;
     [SerializeField] private Transform viewBL;
     [SerializeField] private Transform viewBR;
+    [SerializeField] private Transform arm_link1;
+    [SerializeField] private Transform arm_link2;
+    [SerializeField] private Transform arm_link3;
+    [SerializeField] private Transform arm_link4;
+    [SerializeField] private Transform l_in_link;
+    [SerializeField] private Transform r_in_link;
 
     [SerializeField] private WheelCollider colliderFL;
     [SerializeField] private WheelCollider colliderFR;
@@ -32,14 +38,40 @@ public class CarController : MonoBehaviour
 
     private void GetInput()
     {
-        horizontalInput = Input.GetAxis(HORIZONTAL);
-        verticalInput = Input.GetAxis(VERTICAL);
+        inputs[0] = Input.GetAxis(HORIZONTAL);
+        inputs[1] = Input.GetAxis(VERTICAL);
+        inputs[2] = Input.GetKey(KeyCode.R) ? 1 : (Input.GetKey(KeyCode.F) ? -1 : 0);
+        inputs[3] = Input.GetKey(KeyCode.T) ? 1 : (Input.GetKey(KeyCode.G) ? -1 : 0);
+        inputs[4] = Input.GetKey(KeyCode.Y) ? 1 : (Input.GetKey(KeyCode.H) ? -1 : 0);
+        inputs[5] = Input.GetKey(KeyCode.U) ? 1 : (Input.GetKey(KeyCode.J) ? -1 : 0);
+        inputs[6] = Input.GetKey(KeyCode.I) ? 1 : (Input.GetKey(KeyCode.K) ? -1 : 0);
+    }
+
+    private bool ValidaeAction(float angle, float dir, float min, float max)
+    {
+        if (angle > 180)
+        {
+            angle -= 360;
+        }
+        else if (angle < -180)
+        {
+            angle += 360;
+        }
+        if (0 < dir && (angle < max))
+        {
+            return true;
+        }
+        else if (dir < 0 && min < angle)
+        {
+            return true;
+        }
+        return false;
     }
 
     private void HandleMotor()
     {
-        float currentTurnAngle = maxAngle * horizontalInput;
-        float currentAccelForce = motorForce * verticalInput;
+        float currentTurnAngle = maxAngle * inputs[0];
+        float currentAccelForce = motorForce * inputs[1];
 
         colliderFL.motorTorque = currentAccelForce;
         colliderFR.motorTorque = currentAccelForce;
@@ -51,6 +83,32 @@ public class CarController : MonoBehaviour
         UpdateWheel(colliderFR, viewFR);
         UpdateWheel(colliderBL, viewBL);
         UpdateWheel(colliderBR, viewBR);
+
+        //
+        if (ValidaeAction(arm_link1.localEulerAngles.y - 90, inputs[2], -90, 90))
+        {
+            arm_link1.Rotate(new Vector3(0, inputs[2], 0));
+        }
+        if (ValidaeAction(arm_link2.localEulerAngles.z, inputs[3], -90, 90))
+        {
+            arm_link2.Rotate(new Vector3(0, 0, inputs[3]));
+        }
+        if (ValidaeAction(arm_link3.localEulerAngles.z, inputs[4], -90, 90))
+        {
+            arm_link3.Rotate(new Vector3(0, 0, inputs[4]));
+        }
+        if (ValidaeAction(arm_link4.localEulerAngles.z, inputs[5], -90, 90))
+        {
+            arm_link4.Rotate(new Vector3(0, 0, inputs[5]));
+        }
+        if (ValidaeAction(l_in_link.localEulerAngles.x, inputs[6], 0, 90))
+        {
+            l_in_link.Rotate(new Vector3(inputs[6], 0, 0));
+        }
+        if (ValidaeAction(-r_in_link.localEulerAngles.x, inputs[6], 0, 90))
+        {
+            r_in_link.Rotate(new Vector3(-inputs[6], 0, 0));
+        }
     }
 
     private void UpdateWheel(WheelCollider col, Transform trans)
--- unity_ws/Assets/jetauto_description/JointStatesPublisher.cs
+++ unity_ws/Assets/jetauto_description/JointStatesPublisher.cs
@@ -10,6 +10,17 @@ public class JointStatesPublisher : MonoBehaviour
     [SerializeField] private string topicName = "joint_states";
     [SerializeField] private float publishFrequency = 0.1f;
 
+    [SerializeField] private Transform viewFL;
+    [SerializeField] private Transform viewFR;
+    [SerializeField] private Transform viewBL;
+    [SerializeField] private Transform viewBR;
+    [SerializeField] private Transform arm_link1;
+    [SerializeField] private Transform arm_link2;
+    [SerializeField] private Transform arm_link3;
+    [SerializeField] private Transform arm_link4;
+    [SerializeField] private Transform l_in_link;
+    [SerializeField] private Transform r_in_link;
+
     ROSConnection ros;
     private float timeElapsed = 0.0f;
 
@@ -27,8 +38,23 @@ public class JointStatesPublisher : MonoBehaviour
                 "wheel_right_front_joint",
                 "wheel_left_back_joint",
                 "wheel_right_back_joint",
+                "joint1",
+                "joint2",
+                "joint3",
+                "joint4",
+                "r_joint"
         };
-        joint_states.position = new double[] { 0.0, 0.0, 0.0, 0.0 };
+        joint_states.position = new double[] {
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+        };
     }
 
     private void FixedUpdate()
@@ -45,6 +71,16 @@ public class JointStatesPublisher : MonoBehaviour
                 sec = (int)now,
                 nanosec = (uint)((now - Math.Floor(now)) * Clock.k_NanoSecondsInSeconds)
             };
+            // joint states
+            joint_states.position[0] = viewFL.eulerAngles.x * Mathf.Deg2Rad;
+            joint_states.position[1] = viewFR.eulerAngles.x * Mathf.Deg2Rad;
+            joint_states.position[2] = viewBL.eulerAngles.x * Mathf.Deg2Rad;
+            joint_states.position[3] = viewBR.eulerAngles.x * Mathf.Deg2Rad;
+            joint_states.position[4] = (arm_link1.localEulerAngles.y - 90) * Mathf.Deg2Rad;
+            joint_states.position[5] = arm_link2.localEulerAngles.z * Mathf.Deg2Rad;
+            joint_states.position[6] = arm_link3.localEulerAngles.z * Mathf.Deg2Rad;
+            joint_states.position[7] = arm_link4.localEulerAngles.z * Mathf.Deg2Rad;
+            joint_states.position[8] = l_in_link.localEulerAngles.x * Mathf.Deg2Rad;
             // publish ros topic
             joint_states.header.stamp = stamp;
             ros.Publish(topicName, joint_states);
