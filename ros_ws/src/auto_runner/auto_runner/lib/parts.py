a2add8d9c4bbe8569a7f1ba490d53dfb025726c4
istanadodan <dodan.kwon@gmail.com>
2024-06-11 23:02:26 +0900

    parts 수정중

--- ros_ws/src/auto_runner/auto_runner/lib/parts.py
+++ ros_ws/src/auto_runner/auto_runner/lib/parts.py
@@ -27,7 +27,9 @@ class SensorData:
         cls.tf_sensor = tf_data
         cls.lidar = scan_data
 
-
+class MoveManager:
+    def __init__(self, messagenser: MessageHandler):
+        self.cmd = messagenser
 class RotationManger:
     observers: list[object]
     start_angle: float
@@ -128,6 +130,7 @@ class Policy:
             
         self.policy_plan = _action_map.get(policy)
         self.rcntrler = RobotController()
+        self.rotate_manager = RotationManger(self.rcntrler.cmd)
 
     def apply(self, **kwargs):
         # 3. 단기 목표설정
@@ -147,7 +150,7 @@ class Policy:
         self.apply_result = self.rcntrler.check_rotate_state(**kwargs)
     
 
-class MoveManager:
+class RobotContler:
     def __init__(self):
         # 맵을 가진다.
         self.pathfinder = PathFinder(algorithm='a-star', dest_pos=(0,0))
@@ -161,37 +164,40 @@ class MoveManager:
             self.check_arrived(dest_pos)
 
             # 경로와 방향을 정한다.
-            policy:Policy = self.prepare_policy(dest_pos)
+            policy:Policy = self.make_policy(dest_pos)
+            policy.apply()
 
             return policy.report()
             
         except Exception as e:
+            print(e)
             if e == "arrived":
                 return
             elif e=='no policy':
                 return
-            print(e)
 
-    def prepare_policy(self, dest_pos: tuple) -> PolicyType:
+    def make_policy(self, dest_pos: tuple) -> PolicyType:
         # 가야 할 전략
         _policy:PolicyType = None
         
         # 1. 후진 계획
         if self.pathfinder.is_intrap():
-            self.path = self.pathfinder.back_path()
+            self.pathfinder.back_path()
             _policy = Policy(PolicyType.BACk)
-            return
-
-        # 2. 전진 계획
-        _path = self.pathfinder.find_path(dest_pos)
-
-        if self.check_rotate_policy(_path):
-            _policy = Policy(PolicyType.ROTATE)
         else:
-            _policy = Policy(PolicyType.STRAIGHT)
+
+            # 2. 전진 계획
+            _path = self.pathfinder.find_path(dest_pos)
+
+            if self.check_rotate_policy(_path):
+                _policy = Policy(PolicyType.ROTATE)
+            else:
+                _policy = Policy(PolicyType.STRAIGHT)
+            
+            if _policy == None:
+                raise Exception("no policy")
         
-        if _policy == None:
-            raise Exception("no policy")
+        return _policy
 
 
     def check_arrived(self, dest_pos: tuple):
